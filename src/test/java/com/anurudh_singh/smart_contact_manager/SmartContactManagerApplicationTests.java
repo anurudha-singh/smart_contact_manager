8db868fcfad6ecdb98a5704e125ef0c8c0d17ec8
Anurudha Singh <singhanurudh570@gmail.com>
2024-09-28 09:13:04 +0530

    Initial dependecies added in smart contact manager project

---
+++ src/test/java/com/anurudh_singh/smart_contact_manager/SmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurudh_singh.smart_contact_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
